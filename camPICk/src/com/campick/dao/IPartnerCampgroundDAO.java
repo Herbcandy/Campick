8aa1480fc58362739e798f8660aa1b624bb4a699
LENOVO-YUN\yudong <stellayoon93@gmail.com>
2022-03-11 22:30:30 +0900

    파트너 승인 관련 DAO, DTO

--- camPICk/src/com/campick/dao/IPartnerCampgroundDAO.java
+++ camPICk/src/com/campick/dao/IPartnerCampgroundDAO.java
@@ -2,9 +2,11 @@ package com.campick.dao;
 
 import java.util.ArrayList;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.campick.dto.CampgroundDTO;
+import com.campick.dto.OptionDTO;
 import com.campick.dto.RoomDTO;
 
 public interface IPartnerCampgroundDAO
@@ -30,6 +32,33 @@ public interface IPartnerCampgroundDAO
 	
 	// 객실 추가 
 	public int roomInsert(RoomDTO room);
+
+
+	// 객실 수정
+	public int roomUpdate(RoomDTO room);
+
 	//public int roomInsert(int roomTypeNum, String roomName, int weekDayPrice, int weekEndPrice, int basicNum, int maxNum, String roomInfo );
 	
+	
+	// 캠핑장 수정 시 필요한 캠핑장 정보 가져오기(환불규정 포함)
+	public CampgroundDTO getCampgroundInfoForUpdate(@RequestParam("partnerNum") String partnerNum);
+	
+	// 캠핑장 수정 시 필요한 옵션(편의시설) 정보(OPTIONNUM 포함)
+	public ArrayList<OptionDTO> comfortsListForUpdate(@RequestParam("partnerNum") String partnerNum);
+	
+	// 캠핑장 수정 시 필요한 옵션(즐길거리) 정보(OPTIONNUM 포함)
+	public ArrayList<OptionDTO> funListForUpdate(@RequestParam("partnerNum") String partnerNum);
+	
+	// 환불규정 가이드라인 조회
+	public CampgroundDTO getGuidStandard();
+	
+	// 캠핑장 정보 수정① (옵션 제외 처리)
+	public int modifyCampground(CampgroundDTO campground);
+	
+	// 캠핑장 정보 수정② (옵션 정보 삭제)
+	public int removeOptionStatus(@RequestParam("campgroundId") String campgroundId);
+	
+	// 캠핑장 정보 수정③ (옵션 정보 추가)
+	public int addOptionStatus(@Param("campgroundId") String campgroundId, @Param("optionNum") String optionNum);
+		
 }
